6a198c30d83e1705678790a8e07c4e0fa8fe5894
anandhupraj <anandhupraj1279@gmail.com>
2024-10-19 10:36:21 +0530

    Time: 4 ms (100%), Space: 16.5 MB (93.39%) - LeetHub

--- 3446-find-the-number-of-good-pairs-i/3446-find-the-number-of-good-pairs-i.py
+++ 3446-find-the-number-of-good-pairs-i/3446-find-the-number-of-good-pairs-i.py
@@ -3,7 +3,6 @@ class Solution:
         count=0
         for m,i in enumerate(nums1):
             for n,j in enumerate(nums2):
-                print(m,i)
                 if i%(j*k)==0:
                     count+=1 
         return count
