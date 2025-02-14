bd29c6df500f3b613943475fad6c5c6279c86f3a
anandhupraj <anandhupraj1279@gmail.com>
2025-02-14 15:06:10 +0530

    Time: 0 ms (100%), Space: 18 MB (10.05%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,7 +1,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         # brute force approach
-        while "()" in s or "{}" in s or "[]" in s:
-            s=s.replace("()","").replace("{}","").replace("[]","")
-        return s==""
-        
+        # while "()" in s or "{}" in s or "[]" in s:
+        #     s=s.replace("()","").replace("{}","").replace("[]","")
+        # return s==""
+        #######################################################
+
+        mapping={")":"(","}":"{","]":"["}
+        stack=[]
+        for i in s:
+            if i in mapping:
+                k  = stack.pop() if stack else '#'
+                if mapping[i]!=k:
+                    return False
+            else:
+                stack.append(i)
+        return not stack
