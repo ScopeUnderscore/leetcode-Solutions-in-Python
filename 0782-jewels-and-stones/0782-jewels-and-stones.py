a11f5cfde4fb4f145ed94faca4b9892d294e5c04
anandhupraj <anandhupraj1279@gmail.com>
2025-03-01 11:33:06 +0530

    Time: 0 ms (100%), Space: 17.7 MB (52.15%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -2,10 +2,6 @@ class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         sum=0
         for i in jewels:
-            print(i)
-            # if i in list(stones)
             sum=sum+list(stones).count(i)
-            print(sum)
-
         return sum
         
