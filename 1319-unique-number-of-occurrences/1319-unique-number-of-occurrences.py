6c185dba2e98630bf2dc17ef63f4d5887c0216ea
anandhupraj <anandhupraj1279@gmail.com>
2025-07-08 17:50:56 +0530

    Time: 0 ms (100%), Space: 17.8 MB (65.35%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -1,13 +1,10 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        freq = {}
-        for num in arr:
-            if num in freq:
-                freq[num] += 1
-            else:
-                freq[num] =1
-        # print(freq.values())
-        # print(freq)
-        occurance=list(freq.values())
 
-        return len(occurance)==len(set(occurance))
+        result  = {}
+        for i in arr:
+            result[i] =result.get(i, 0) + 1
+        print(result)
+        occurance = list(result.values())
+
+        return len(occurance) == len(set(occurance)) 
