d40f7d361e48e793eb30a465e7ae4ad682f62db4
anandhupraj <anandhupraj1279@gmail.com>
2025-08-07 13:38:08 +0530

    Time: 13 ms (16.31%), Space: 17.9 MB (31.94%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
@@ -1,12 +1,20 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
         dic={}
+        # for i in range(len(arr)):
+        #     for j in range(0,len(arr)):
+        #         print(i,j)
+        #         if i!=j and arr[i] == 2*arr[j]:
+        #             return True
+        # return False   
+
         for i in range(len(arr)):
-            for j in range(0,len(arr)):
-                print(i,j)
-                if i!=j and arr[i] == 2*arr[j]:
-                    return True
-        return False      
+            if arr[i] * 2 in dic or arr[i]/2 in dic:
+                return True
+            dic[arr[i]]=0
+            print(dic)
+        return False
+                
                 
                 
             
